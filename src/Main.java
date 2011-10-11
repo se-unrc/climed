501733c1a0b99dde51aab03a608f683ed073edd5
Agvarea <agvarea@gmail.com>
2011-10-11 11:55:27 -0300

    Agregar ventana principal

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@ public class Main {
 
 	public static void main(String args[]) {
 		System.out.println("hola mundo");
+		@SuppressWarnings("unused")
+		Mediador_Principal med = new Mediador_Principal();
 	}
 
 }
