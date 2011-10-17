75360915461feb88010286ec3c7cd6f1e3874e53
octicoco <octicoco@gmail.com>
2011-10-17 19:26:55 -0300

    Subida de medicamentos G1 (a ver si esta anda)

---
+++ src/medicamentos/Main.java
@@ -0,0 +1,11 @@
+// Usar para correr el módulo Medicamentos
+
+package medicamentos;
+
+public class Main {
+
+    public static void main(String[] args) {
+        MedMedicamentos med = new MedMedicamentos();
+    }
+
+}
