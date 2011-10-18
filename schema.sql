49f3da0c6b7b79eb87f3f9308ce6cc9fff729adc
Agvarea <agvarea@gmail.com>
2011-10-18 12:57:57 -0300

    Agrega esquema de la tabla convenio

---
+++ schema.sql
@@ -0,0 +1,8 @@
+﻿drop TABLE if EXISTS Convenio;
+create TABLE  convenio (
+  nro_convenio serial NOT NULL ,
+  medico varchar(30) NOT NULL,
+  obra_social varchar(30) NOT NULL,
+  cobertura float NOT NULL,
+  Constraint pkConvenio PRIMARY KEY (medico,obra_social)
+ ) ;
