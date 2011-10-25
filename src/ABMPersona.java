cc8a29f647d5ae804b514461d26683231e4f56e2
prodeghiero <pablorodeghiero@gmail.com>
2011-10-25 11:51:39 -0300

    Arma querry y ejecuta querry

--- src/ABMPersona.java
+++ src/ABMPersona.java
@@ -1,6 +1,10 @@
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.LinkedList;
+import java.util.List;
 
 public class ABMPersona{
 	//private static Conexion conexion;
@@ -13,8 +17,47 @@ public class ABMPersona{
 		Conexion.cerrarConexion();
 	}
 	
-	public static Persona levantarBaseDatos(){
-		Persona tupla = new Persona();
-		return tupla;
+	public static String armarQuerry(String comando,String tabla, String[] values, String[] where){
+		String retorno = "";
+		comando = comando.toLowerCase();
+		if (comando.equals("insert")){
+			retorno += "INSERT INTO " + tabla + " VALUES (";
+			for (int i =0; i<values.length;i++){
+				retorno += values[i];
+				if (i<values.length-1){
+					retorno += ", ";
+				}
+			}
+			retorno += ")";
+		}else if(comando.equals("delete")){
+			retorno = "Delete from " + tabla + " WHERE ";
+			for (int i = 0; i< where.length ; i++ ){
+				retorno += "(" + where[i] + ")" ;
+				if (i<where.length-1){
+					retorno += " and ";
+				}
+			}
+			retorno += ")";
+		}else if (comando.equals("update")){
+			retorno = "UPDATE " + tabla + " SET ";
+			for (int i = 0; i<values.length; i++){
+				retorno += values[i];
+				if (i<values.length-1){
+					retorno +=" , ";
+				}
+			}
+			retorno += " WHERE ";
+			for (int i = 0; i<where.length;i++){
+				retorno += " (" + values[i]+ " ) ";
+				if (i<where.length-1){
+					retorno += " AND ";
+				}
+			}
+			retorno += ")";
+		}else{
+			System.out.println("error comando incorrecto");
+			return "";
+		}
+		return retorno;
 	}
 }
--- src/Main.java
+++ src/Main.java
@@ -5,17 +5,6 @@ public class Main {
 		//@SuppressWarnings("unused")
 		//Mediador_Principal med = new Mediador_Principal();
 		//MediadorGuiPersona a = new MediadorGuiPersona();
-		try{
-			ABMPersona.ABM("Delete FROM hora_extra WHERE (numero=9)AND (horas=9) and (fecha='1')");
-			ABMPersona.ABM("INSERT INTO hora_extra VALUES (9,9,'1')");
-			ABMPersona.ABM("UPDATE hora_extra SET numero=18 WHERE (numero=9)AND (horas=9) and (fecha='1')");
-			ABMPersona.ABM("INSERT INTO hora_extra VALUES (9,9,'1')");
-		
-		}
-		catch(Exception e){
-			e.printStackTrace();
-			System.out.println("se re rompio");
-		}
 	}
 
 }
