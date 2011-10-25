bcd7facbdb7511e2d0d3bd8f754225d486e17fb6
Franco Brusatti <fbrusatti@gmail.com>
2011-10-25 11:53:30 -0300

    Add Medicamento to Schema

--- medicamentos/medicamento.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE medicamento (
-    codigo bigint NOT NULL,
-    nombre text,
-    stock double precision,
-    stock_min double precision,
-    vencimiento date,
-    unidad text,
-	CONSTRAINT pkCodigo PRIMARY KEY (codigo)
-);
