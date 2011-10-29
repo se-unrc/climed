7b0dc72aa8d1e4f9736740d5dab4e95c6d8396f7
Franco Brusatti <fbrusatti@gmail.com>
2011-10-29 11:22:42 -0300

    Se arregla el schema de la BD

--- schema.sql
+++ schema.sql
@@ -1,16 +1,19 @@
-﻿drop TABLE if EXISTS Convenio;
-create TABLE  convenio (
+﻿CREATE SEQUENCE seq_id_convenio;
+DROP TABLE IF EXISTS convenio;
+CREATE TABLE  convenio (
+  id integer NOT NULL DEFAULT NEXTVAL('seq_id_convenio'),
   nro_convenio serial NOT NULL ,
   medico varchar(30) NOT NULL,
   obra_social varchar(30) NOT NULL,
   cobertura float NOT NULL,
-  Constraint pkConvenio PRIMARY KEY (medico,obra_social)
+  Constraint pkConvenio PRIMARY KEY (id)
  ) ;
 
 
-drop TABLE if EXISTS Medicamento;
-CREATE TABLE Medicamento (
-    id bigint NOT NULL,
+CREATE SEQUENCE seq_id_medicamento;
+DROP TABLE IF EXISTS medicamento;
+CREATE TABLE medicamento (
+    id bigint NOT NULL DEFAULT nextval('seq_id_medicamento'),
     nombre text,
     stock double precision,
     stock_min double precision,
@@ -18,4 +21,15 @@ CREATE TABLE Medicamento (
     unidad text,
     CONSTRAINT pkCodigo PRIMARY KEY (id)
 );
+
+CREATE SEQUENCE seq_id_consulta;
+DROP TABLE IF EXISTS consulta;
+CREATE TABLE consulta(
+  id integer NOT NULL DEFAULT nextval('seq_id_consulta'),
+  idMedico integer NOT NULL,
+  idPaciente integer NOT NULL,
+  fecha date NOT NULL,
+  diagnostico character varying(200),
+  CONSTRAINT pkconsulta PRIMARY KEY (id)
+)
 
