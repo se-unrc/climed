16f5f20206875c496cc439f173d251578efe7cf9
Mane <mane@mane-HP-Pavilion-dv5-Notebook-PC.(none)>
2011-11-04 17:30:39 -0300

    Grupo5 Horarios Final

---
+++ src/Propio/Main.java
@@ -0,0 +1,15 @@
+package Propio;
+
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MediadorPrincipal med = new MediadorPrincipal();
+
+	}
+
+}
