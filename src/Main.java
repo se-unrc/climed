945e62d5ffd30f9398756ac0075fdaf8c5637462
Franco Brusatti <fbrusatti@gmail.com>
2011-11-06 17:50:23 -0300

    Twitter Web Service connection

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.ClientConfig;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
+
 public class Main {
 	
 	public static void main(String args[]) {
+		WebResource wr;
+                // Friends del tucu
+                final String BaseURI = "https://api.twitter.com/1/friends/ids.json?screen_name=tucumetal";
+                // ultimo status del tucu
+		// final String BaseURI = "http://twitter.com/statuses/user_timeline/tucumetal.json?count=1";
+
+		// Config client
+		ClientConfig config = new DefaultClientConfig();
+
+		// Create client
+		Client client = Client.create(config);
+		wr = client.resource(BaseURI);
+
+		// do the get to twitter
+		String s = wr.get(String.class);
+		System.out.println(s);
+
 		try{
 			@SuppressWarnings("unused")
 			Mediador_Principal med = new Mediador_Principal();
@@ -9,5 +31,4 @@ public class Main {
 			e.printStackTrace();
 		}
 	}
-
 }
